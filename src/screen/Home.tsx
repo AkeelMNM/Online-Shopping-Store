08e74aa8456602d6ccda9ae127f501191206b874
Mohamed Akeel <akeel1447@gmail.com>
2022-12-07 17:03:12 +0530

    Adding styles to home screen

--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import ShoppingCart from '../assets/images/ShoppingCart.png';
+import { HomeCard } from '../components';
 
 const carouselImage = [
 	'https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80',
@@ -13,7 +14,7 @@ const Home = () => {
 	return (
 		<div>
 			<div className="flex flex-row justify-between">
-				<label className="p-5 font-serif text-2xl font-semibold">
+				<label className="p-5 font-serif text-4xl font-semibold">
 					Fashion Store
 				</label>{' '}
 				<img
@@ -38,6 +39,47 @@ const Home = () => {
 						);
 					})}
 				</Carousel>
+			</div>
+			<div className="flex flex-row justify-between p-10">
+				<HomeCard
+					title="Support Local"
+					description="All the materials are come from local producers. Together we can create a strong and better community."
+					image="https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80"
+				/>
+				<HomeCard
+					title="High quality"
+					description="Martials are high quality proves to last many years."
+					image="https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80"
+				/>
+				<HomeCard
+					title="Eco friendly"
+					description="Rest assures product and materiels used will never harm our environment."
+					image="https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80"
+				/>
+			</div>
+			<div className="flex flex-col place-items-center">
+				<label className="p-5 font-serif text-2xl font-semibold">
+					Best Seller
+				</label>
+				<div className="flex flex-row p-10">
+					<HomeCard
+						title="Support Local"
+						description="All the materials are come from local producers. Together we can create a strong and better community."
+						image="https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80"
+					/>
+				</div>
+			</div>
+			<div className="flex flex-col place-items-center">
+				<label className="p-5 font-serif text-2xl font-semibold">
+					Trusted by our partners
+				</label>
+				<div className="flex flex-row p-10">
+					<HomeCard
+						title="Support Local"
+						description="All the materials are come from local producers. Together we can create a strong and better community."
+						image="https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80"
+					/>
+				</div>
 			</div>
 		</div>
 	);
